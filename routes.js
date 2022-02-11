20843f5946f2c87553dd00bdc571aa8539288cd5
Atik Mandhat <atikmandhat86@gmail.com>
2022-02-11 16:32:23 +0530

    Reatc file project commit

---
+++ routes.js
@@ -0,0 +1,9 @@
+// import Signin from "./src/layout/authentication/signin";
+
+// const routes=[
+//     type: "collapse",
+//     name: "Sign In",
+//     key: "signin",
+//     route: "/signin",
+//     component: <Signin />,"
+// ]
--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Signin from './layout/authentication/signin';
+import NavHeader from './layout/dashboard/components/navheader'
+import Footer from './layout/dashboard/components/footer'
+import Books from './layout/books';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <NavHeader/>
+        <Signin/>
+        <Books/>
+        <Footer/>
       </header>
     </div>
   );
 }
-
 export default App;
